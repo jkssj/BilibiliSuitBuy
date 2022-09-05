39f0f468405f7bf0ed02dfb2791749e4447f1ab5
好好好 <73286581+lllk140@users.noreply.github.com>
2022-09-06 01:59:36 +0800

    Add files via upload

---
+++ tools/config.py
@@ -0,0 +1,24 @@
+a_config = {
+    "cookies": {
+        "SESSDATA": "xxxxxxxxxxxxxx",
+        "bili_jct": "xxxxxxxxxxxxxx",
+        "DedeUserID": "xxxxxxxxxxxxxx", "sid": "xxxxxxxxxxxxxx",
+        "DedeUserID__ckMd5": "xxxxxxxxxxxxxx",
+        "Buvid": "xxxxxxxxxxxxxx"
+    },
+    "bili_eid": "xxxxxxxxxxxxxx",
+    "access_key": "xxxxxxxxxxxxxx",
+    "app_key": "1d8b6e7d45233436",
+
+    "item_id": "xxxxxxxxxxxxxx",
+    "system_version": "9",
+    "channel": "yingyongbao",
+    "sdk_int": "28",
+    "version": "6.88.0",
+    "buv_id": "xxxxxxxxxxxxxx",
+    "phone": "SM-G9810",
+    "build": "6880300",
+
+    "proxies": {"http": None, "https": None},
+    "trust_env": False
+}
