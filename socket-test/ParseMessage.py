88f74755b9f97006c756a4c60d2a55abda094918
好好好 <73286581+lllk140@users.noreply.github.com>
2022-09-11 08:56:24 +0800

    Update ParseMessage.py

--- socket-test/ParseMessage.py
+++ socket-test/ParseMessage.py
@@ -1,21 +1,19 @@
 from urllib.parse import urlsplit
+from urllib.parse import unquote
 
 
 def ParseHttpMessage(content: bytes):
-    body_byte_array, value_dict = content.split(b"\r\n"), dict()
-
-    body_split = [tuple(i.split(b": ")) for i in body_byte_array[1:]]
+    body_array, value_dict = content.split(b"\r\n"), dict()
+    body_split = [tuple(i.split(b": ")) for i in body_array[1:]]
     body_dict = dict()
     for body_key in body_split:
         key = body_key[0].decode().lower()
-        if 1 < len(body_key) < 3:
-            value = body_key[1].decode()
-            body_dict[key] = value
-        else:
-            body_dict[key] = None
+        value = list(body_key)[1:]
+        value = b"".join(value).decode()
+        body_dict[key] = value
 
     # url—path
-    message_url = body_byte_array[0].split(b" ")[1]
+    message_url = body_array[0].split(b" ")[1]
     urlsplit_text = urlsplit(message_url.decode("utf-8"))
     verify_path = urlsplit_text.path == "/x/garb/v2/mall/suit/detail"
     assert verify_path, "/x/garb/v2/mall/suit/detail not in HttpMessage"
@@ -28,12 +26,8 @@ def ParseHttpMessage(content: bytes):
     value_dict.update({"access_key": access_key})
 
     app_key = params_dict.get("appkey", None)
-    assert app_key, "app_key not in HttpMessage"
-    value_dict.update({"app_key": app_key})
-
-    csrf = params_dict.get("csrf", None)
-    assert csrf, "csrf not in HttpMessage"
-    value_dict.update({"csrf": csrf})
+    assert app_key, "appkey not in HttpMessage"
+    value_dict.update({"appkey": app_key})
 
     item_id = params_dict.get("item_id", None)
     assert item_id, "item_id not in HttpMessage"
@@ -41,13 +35,14 @@ def ParseHttpMessage(content: bytes):
 
     statistics = params_dict.get("statistics", None)
     assert statistics, "statistics not in HttpMessage"
-    value_dict.update({"statistics": statistics})
+    value_dict.update({"statistics": unquote(statistics)})
 
     # cookie
     cookie_message = body_dict.get("cookie", None)
     assert cookie_message, "cookie not in HttpMessage"
     value_dict.update({"cookie": cookie_message})
 
+    # User-Agent
     user_agent = body_dict.get("user-agent", None)
     assert user_agent, "User-Agent not in HttpMessage"
     value_dict.update({"user-agent": user_agent})
@@ -56,11 +51,6 @@ def ParseHttpMessage(content: bytes):
     aurora_eid = body_dict.get("x-bili-aurora-eid", None)
     assert aurora_eid, "x-bili-aurora-eid not in HttpMessage"
     value_dict.update({"x-bili-aurora-eid": aurora_eid})
-
-    # x-bili-mid
-    bili_mid = body_dict.get("x-bili-mid", None)
-    assert bili_mid, "x-bili-mid not in HttpMessage"
-    value_dict.update({"x-bili-mid": bili_mid})
 
     return value_dict
 
