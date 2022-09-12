6a9c3d63c03c24fa42e4e82e3e616d5f59741abc
好好好 <73286581+lllk140@users.noreply.github.com>
2022-09-12 16:11:10 +0800

    Update speed-test-golang.go

--- socket-test/speed-test-golang.go
+++ socket-test/speed-test-golang.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	var message string = "GET https://api.bilibili.com/x/report/click/now HTTP/1.1\r\nHost: api.bilibili.com\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33\n\r\nAccept: text/html\r\nAccept-Encoding: gzip, deflate\r\n\r\n"
+	var message string = "GET https://api.bilibili.com/x/report/click/now HTTP/1.1\r\nHost: api.bilibili.com\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33\r\nAccept: text/html\r\nAccept-Encoding: gzip, deflate\r\n\r\n"
 	client, _ := net.Dial("tcp", "api.bilibili.com:80")
 	var result []uint8 = make([]byte, 4096)
 	time.Sleep(2 * time.Second)
