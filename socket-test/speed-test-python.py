300726ef5305c250a487014dfdff5b71207bb80c
好好好 <73286581+lllk140@users.noreply.github.com>
2022-09-12 16:11:27 +0800

    Update speed-test-python.py

--- socket-test/speed-test-python.py
+++ socket-test/speed-test-python.py
@@ -4,7 +4,7 @@ import ssl
 
 
 def main():
-    message = "GET https://api.bilibili.com/x/report/click/now HTTP/1.1\r\nHost: api.bilibili.com\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33\n\r\nAccept: text/html\r\nAccept-Encoding: gzip, deflate\r\n\r\n"
+    message = "GET https://api.bilibili.com/x/report/click/now HTTP/1.1\r\nHost: api.bilibili.com\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33\r\nAccept: text/html\r\nAccept-Encoding: gzip, deflate\r\n\r\n"
     client = ssl.wrap_socket(socket.socket())
     client.connect(("api.bilibili.com", 443))
     start_time = time.time() * 1000
