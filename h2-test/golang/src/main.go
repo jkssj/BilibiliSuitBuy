0e886efb2a1f64020605b77a55b9a00af5d5fa2d
afuet <73286581+lllk140@users.noreply.github.com>
2022-10-20 13:47:09 +0800

    Update main.go

--- h2-test/golang/src/main.go
+++ h2-test/golang/src/main.go
@@ -4,68 +4,50 @@ import (
 	"crypto/tls"
 	"fmt"
 	"golang.org/x/net/http2/hpack"
-	"golang/src/TH2"
+	"hh2/src/TH2"
 )
 
 func main() {
-	//var Settings = TH2.NewSettingsFrames()
-	//var body = TH2.BuildSettingsFrames(Settings)
-	//var data = new(TH2.H2Connection)
-	//data.InitiateConnection(nil)
-	//fmt.Printf("%v\n", body)
 
 	var TlsConfig *tls.Config = &tls.Config{
 		NextProtos: []string{"h2"},
 	}
+
 	var con, _ = tls.Dial("tcp", "api.bilibili.com:443", TlsConfig)
 
-	var h2 = new(TH2.H2Connection)
-	h2.InitiateConnection(nil)
-	var InitData = h2.DataToSend()
+	var th2 = new(TH2.H2Connection)
 
-	fmt.Printf("InitData: %v\n", InitData)
-	var _, _ = con.Write(InitData)
+	th2.InitiateConnection()
+	//var InitData = th2.DataToSend()
+	//_, _ = con.Write(InitData)
+	//fmt.Printf("%v\n", InitData)
+
+	th2.SendSettings(nil)
+	var SettingsData = th2.DataToSend()
+	_, _ = con.Write(SettingsData)
+	fmt.Printf("%v\n", SettingsData)
 
 	var headers = []hpack.HeaderField{
 		{Name: ":method", Value: "GET"},
 		{Name: ":path", Value: "/x/space/acc/info?mid=1701735549"},
 		{Name: ":authority", Value: "api.bilibili.com"},
 		{Name: ":scheme", Value: "https"},
-		{Name: "User-Agent", Value: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"},
+		//{Name: "User-Agent", Value: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"},
 	}
-	h2.SendHeaders(1, headers, true)
 
-	var HeaderData = h2.DataToSend()
-	fmt.Printf("HeaderData: %v\n", HeaderData)
-	_, _ = con.Write(HeaderData)
+	th2.SendHeaders(1, headers, 5)
+	var HeadersData = th2.DataToSend()
+	_, _ = con.Write(HeadersData)
 
-	//h2.SendData(1, []byte("headers"), true)
-	//h2.DataToSend()
-	for {
+	fmt.Printf("%v\n", HeadersData)
+
+	for i := 0; i < 4; i++ {
 		var buf = make([]byte, 4096)
-
-		var ReadLength, _ = con.Read(buf)
-
-		//fmt.Printf("%v\n", buf[:ReadLength])
-
-		//fmt.Printf("%v\n", buf)
-		fmt.Printf("data: %v\n", buf[:ReadLength])
-
-		var events = h2.ReceiveData(buf[:ReadLength])
-		for _, event := range events {
-			fmt.Printf("%v\n", event)
-			if event.FrameType == 3 {
-				goto EXIT
-			}
-			if event.FrameType == 0 {
-				goto EXIT
-			}
+		var length, _ = con.Read(buf)
+		var data = buf[:length]
+		fmt.Printf("data:%v\n", data)
+		if i == 0 {
+			_, _ = con.Write([]byte{0, 0, 0, 4, 1, 0, 0, 0, 0})
 		}
-		var data = h2.DataToSend()
-		fmt.Printf("DataToSend: %v\n", data)
-		_, _ = con.Write(data)
 	}
-
-EXIT:
-	fmt.Printf("%v\n", "end")
 }
