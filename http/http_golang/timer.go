59495c7b72d5ef4acedc018b5bccafb43cb5aa8e
afuet <73286581+lllk140@users.noreply.github.com>
2022-11-19 12:55:03 +0800

    Update timer.go

--- http/http_golang/timer.go
+++ http/http_golang/timer.go
@@ -64,7 +64,7 @@ func (receiver *BiliTimer) WaitLocalTime(jump int64) float64 {
 }
 
 func (receiver *BiliTimer) WaitSeverTime() float64 {
-	var SleepTimeNumber = receiver.DelayTime * float64(time.Second)
+	var SleepTimeNumber = (receiver.DelayTime / 1000) * float64(time.Second)
 	receiver.updateClient()
 	var NowTime = receiver.GetBiliTime()
 	for NowTime < receiver.SaleTime {
